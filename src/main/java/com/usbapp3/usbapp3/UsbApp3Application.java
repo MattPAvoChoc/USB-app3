23241b35576e51ca0c7cee7cec75e4fa04aa073a
MattPAvoChoc <matthewpearce@avochoc.com>
2022-06-06 13:24:10 -0700

    Initial commit

---
+++ src/main/java/com/usbapp3/usbapp3/UsbApp3Application.java
@@ -0,0 +1,13 @@
+package com.usbapp3.usbapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsbApp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsbApp3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/usbapp3/usbapp3/UsbApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usbapp3.usbapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsbApp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
